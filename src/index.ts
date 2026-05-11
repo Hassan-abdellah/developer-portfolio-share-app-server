6525aba2af4487588198eb024019e073d3bc122c
Hassan Abdellah <hassanabdellah25@gmail.com>
2026-05-11 20:21:41 +0300

    implement user clerk webhook on created update and delete events

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import "dotenv/config";
 import express from "express";
+import userRouter from "./routes/users.js";
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+
+app.use("/webhooks/clerk", userRouter);
 
 app.listen(PORT, () => console.log(`Server is Running on PORT ${PORT}`));
